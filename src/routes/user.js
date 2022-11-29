50463bf908d988b2417cfdbdff0e83b3edba57dd
theluposki <theluposki@gmail.com>
2022-11-28 22:16:41 -0300

    middleware auth

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/UserController.js'
 
+import Auth from "../middleware/auth.js"
+
 const router = Router()
 
 router.post('/auth', UserController.auth)
 router.post('/', UserController.registerUser)
-router.get('/', UserController.getUsers)
+router.get('/', Auth, UserController.getUsers)
 
 export default router
